685224c9d81de2f64ce472d8380a5a0115717f08
Pablogabi07 <83091813+Pablogabi07@users.noreply.github.com>
2025-11-18 11:50:40 -0300

    Update DrinkCard.js

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,20 +1,24 @@
 import React from "react";
 
-const DrinkCard = ({ name, emoji, price, addToCart }) => {
-  const message = `Hola Tropichicas! Quiero pedir un ${name} ${emoji}`;
+const DrinkCard = ({ name, price, image, emoji, addToCart }) => {
+  const message = `Hola Tropichicas! Quiero pedir un ${name}`;
   const wspLink = `https://wa.me/5491134264038?text=${encodeURIComponent(
     message
   )}`;
 
   return (
     <div className="drink-card">
-      <span className="emoji">{emoji}</span>
+      {image ? (
+        <img src={image} alt={name} className="drink-photo" />
+      ) : (
+        <span className="emoji">{emoji}</span>
+      )}
       <p>{name}</p>
       <p className="price">💲{price}</p>
 
       <button
         className="btn-add"
-        onClick={() => addToCart({ name, emoji, price })}
+        onClick={() => addToCart({ name, price, image, emoji })}
       >
         Agregar
       </button>
